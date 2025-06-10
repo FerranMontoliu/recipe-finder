ade8acd27414525da8f88ba2f37edf4b2239874e
Ferran Montoliu Boneu <f.montoliu@flexidao.com>
2025-06-10 21:58:13 +0200

    feat: Created empty app.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import '@mantine/core/styles.css'
+
+import { type ReactElement } from 'react'
+
+export const App = (): ReactElement => <>App goes here</>
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme, type MantineThemeOverride } from '@mantine/core'
+
+export const theme: MantineThemeOverride = createTheme({
+  primaryColor: 'yellow',
+  black: '#1f1f1f',
+  white: '#fdfdfd',
+})
