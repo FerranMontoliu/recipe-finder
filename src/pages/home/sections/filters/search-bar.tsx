520d70fd7a67c30875bf0f8b60802eba6462dd0e
Ferran Montoliu Boneu <f.montoliu@flexidao.com>
2025-06-12 11:12:46 +0200

    feat: Updated the search bar to be clearable.

--- src/pages/home/sections/filters/search-bar.tsx
+++ src/pages/home/sections/filters/search-bar.tsx
@@ -1,28 +1,58 @@
-import type { ReactElement } from 'react'
-import { TextInput } from '@mantine/core'
-import { IconSearch } from '@tabler/icons-react'
+import { type ReactElement, useEffect, useState } from 'react'
+import { ActionIcon, TextInput } from '@mantine/core'
+import { IconSearch, IconX } from '@tabler/icons-react'
 
 interface SearchBarProps {
   query: string
   onQueryChange: (query: string) => void
   disabled?: boolean
 }
+
 export const SearchBar = ({
   query,
   onQueryChange,
   disabled = false,
-}: SearchBarProps): ReactElement => (
-  <TextInput
-    placeholder="Search by recipe name..."
-    leftSection={<IconSearch size={16} />}
-    radius="md"
-    size="md"
-    w="100%"
-    defaultValue={query}
-    onChange={(event) => {
-      onQueryChange(event.currentTarget.value)
-    }}
-    disabled={disabled}
-    test-id="search-bar-input"
-  />
-)
+}: SearchBarProps): ReactElement => {
+  const [inputValue, setInputValue] = useState(query)
+
+  useEffect(() => {
+    setInputValue(query)
+  }, [query])
+
+  const onChange = (newValue: string) => {
+    setInputValue(newValue)
+    onQueryChange(newValue)
+  }
+
+  const onClear = () => {
+    setInputValue('')
+    onQueryChange('')
+  }
+
+  return (
+    <TextInput
+      placeholder="Search by recipe name..."
+      leftSection={<IconSearch size={16} />}
+      rightSection={
+        inputValue && !disabled ? (
+          <ActionIcon
+            variant="subtle"
+            onClick={onClear}
+            aria-label="Clear search"
+          >
+            <IconX size={16} />
+          </ActionIcon>
+        ) : null
+      }
+      radius="md"
+      size="md"
+      w="100%"
+      value={inputValue}
+      onChange={(event) => {
+        onChange(event.currentTarget.value)
+      }}
+      disabled={disabled}
+      data-testid="search-bar-input"
+    />
+  )
+}
